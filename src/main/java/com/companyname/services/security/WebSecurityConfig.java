88ff0fb34a63c2c79b77b3459a4639f13db2a90a
daenis <dkalaygia@gmail.com>
2024-01-13 15:48:35 -1000

    Updated

---
+++ src/main/java/com/companyname/services/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.companyname.services.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public static final String APP_USER = "app-user";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> {
+            authorizeRequests
+                    .requestMatchers(HttpMethod.GET, "/health").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/v1/**").hasAnyRole(APP_USER)
+                    .anyRequest().authenticated();
+        });
+        http.oauth2ResourceServer(resourceServer -> {
+            resourceServer.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter));
+        });
+        http.sessionManagement(sessionManagement -> {
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+        return http.build();
+    }
+}
